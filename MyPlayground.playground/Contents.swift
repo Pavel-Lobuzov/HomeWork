23d983193d2342615b1224fd43b20402408b1f05
Павел Лобузов <pavel.lobuzov@icloud.com>
2025-07-14 01:32:19 +0300

    1 task

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//1
+
+func greeting() {
+    print("hello world")
+}
+greeting()
+//
+func sayHello(_ name: String) {
+    print("Привет \(name)")
+}
+print(sayHello("Паша"))
+//
+func sayHello2 (_ name: String = "Паша") -> String   {
+    return String ("Привет! \(name)")
+}
+print(sayHello2())
